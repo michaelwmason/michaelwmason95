15eb64ac41fd43922580b4241e366bf8fa123e30
Michael Mason <michael.mason7194@gmail.com>
2021-03-06 23:58:30 -0500

    Updated devops init script

--- opensource/terraform_components/devops_server/init.sh
+++ opensource/terraform_components/devops_server/init.sh
@@ -5,4 +5,4 @@ sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 sudo apt install git-all -y
-sudo apt install xclip -y
+ansible-galaxy collection install amazon.aws
