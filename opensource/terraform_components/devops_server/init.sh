9e03bedcfe9c12b29c5f5bd9ce53a8caf5d74a1e
Michael Mason <michael.mason7194@gmail.com>
2021-03-08 18:02:59 -0500

    Fixed typo

--- opensource/terraform_components/devops_server/init.sh
+++ opensource/terraform_components/devops_server/init.sh
@@ -6,7 +6,7 @@ sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 sudo apt install git-all -y
-sudo apt install python3-pip -yes
+sudo apt install python3-pip -y
 pip3 install boto3
 ansible-galaxy collection install amazon.aws
 ansible-galaxy collection install community.aws
