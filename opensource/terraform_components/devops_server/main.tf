88af16f75fc601479d8eca51f26bbf30ef55410f
Michael Mason <michael.mason7194@gmail.com>
2021-03-13 20:29:40 -0500

    Format

--- opensource/terraform_components/devops_server/main.tf
+++ opensource/terraform_components/devops_server/main.tf
@@ -17,10 +17,10 @@ provider "aws" {
 }
 
 data "terraform_remote_state" "devops_server_instance_profile" {
-   backend = "s3"
+  backend = "s3"
   config = {
     bucket = "michaelwmason-terraform"
-    key = "devops_server_role"
+    key    = "devops_server_role"
     region = "us-east-1"
   }
 }
@@ -45,16 +45,16 @@ resource "aws_security_group" "devops_server" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["${local.ifconfig_co_json.ip}/32"]
-    self = true
+    self        = true
   }
 
-   ingress {
-     description = "GO CD dashboard"
+  ingress {
+    description = "GO CD dashboard"
     from_port   = 8153
     to_port     = 8153
     protocol    = "tcp"
     cidr_blocks = ["${local.ifconfig_co_json.ip}/32"]
-    self = true
+    self        = true
   }
 
   egress {
@@ -82,7 +82,7 @@ resource "aws_instance" "devops_server" {
     Name = "DevOpsServer"
   }
   iam_instance_profile = data.terraform_remote_state.devops_server_instance_profile.outputs.id
-  security_groups = [aws_security_group.devops_server.name]
-  user_data       = data.template_file.init.rendered
+  security_groups      = [aws_security_group.devops_server.name]
+  user_data            = data.template_file.init.rendered
 }
 
