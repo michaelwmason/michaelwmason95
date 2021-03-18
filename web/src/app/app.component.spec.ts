3b5771335336abe482fb0f52b8775c2b337c1936
Michael Mason <michael.mason7194@gmail.com>
2021-03-18 18:11:07 -0400

    Updated web to not use commas

--- web/src/app/app.component.spec.ts
+++ web/src/app/app.component.spec.ts
@@ -1,35 +1,29 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from '@angular/core/testing'
+import { ReactiveFormsModule } from '@angular/forms'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from './app.component'
+import { EmailService } from './services/email.service/email.service'
+import {
+    HttpClientTestingModule,
+    HttpTestingController,
+} from '@angular/common/http/testing'
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                ReactiveFormsModule,
+                HttpClientTestingModule,
+            ],
+            declarations: [AppComponent],
+            providers: [EmailService, HttpTestingController],
+        }).compileComponents()
+    }))
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'web'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('web app is running!');
-  });
-});
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent)
+        const app = fixture.componentInstance
+        expect(app).toBeTruthy()
+    })
+})
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl:'/api'
-};
+    production: false,
+    baseUrl: '/api',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
