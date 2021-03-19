7a4b97221aa74e65b77ba450a67b8bb766112b2d
Michael Mason <michael.mason7194@gmail.com>
2021-03-19 13:23:44 -0400

    Updating linting and styling

--- web/src/app/app.component.spec.ts
+++ web/src/app/app.component.spec.ts
@@ -1,29 +1,29 @@
-import { TestBed, async } from '@angular/core/testing'
-import { ReactiveFormsModule } from '@angular/forms'
-import { RouterTestingModule } from '@angular/router/testing'
-import { AppComponent } from './app.component'
-import { EmailService } from './services/email.service/email.service'
+import { TestBed, async } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { EmailService } from './services/email.service/email.service';
 import {
-    HttpClientTestingModule,
-    HttpTestingController,
-} from '@angular/common/http/testing'
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                RouterTestingModule,
-                ReactiveFormsModule,
-                HttpClientTestingModule,
-            ],
-            declarations: [AppComponent],
-            providers: [EmailService, HttpTestingController],
-        }).compileComponents()
-    }))
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+      ],
+      declarations: [AppComponent],
+      providers: [EmailService, HttpTestingController],
+    }).compileComponents();
+  }));
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        const app = fixture.componentInstance
-        expect(app).toBeTruthy()
-    })
-})
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
