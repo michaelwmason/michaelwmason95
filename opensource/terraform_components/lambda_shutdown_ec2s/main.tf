ef3851298f5ce78640dc977766ad6a318a08cc1d
Michael Mason <michael.mason7194@gmail.com>
2021-04-02 11:35:48 -0400

    Updated lambda and cloudwatch to shutdown ec2s at 2:00 AM to save on costs

--- opensource/terraform_components/lambda_shutdown_ec2s/main.tf
+++ opensource/terraform_components/lambda_shutdown_ec2s/main.tf
@@ -16,18 +16,71 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "archive_file" "lambda" {
+  type = "zip"
+  source_file = "${path.cwd}/index.py"
+  output_path = "${path.cwd}/index.zip"
+}
+
 
 resource "aws_lambda_function" "shutdown_ec2s" {
-  filename      = "lambda_function.zip"
   function_name = "ShutDown_Ec2s"
-  role          = aws_iam_role.iam_for_lambda.arn
+  filename      = "index.zip"
   handler       = "index.lambda_handler"
-
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
-
+  memory_size   = 256
+  source_code_hash = data.archive_file.lambda.output_base64sha256
   runtime = "python3.8"
+  role = aws_iam_role.lambda_shutdown_ec2s_role.arn
+}
 
+resource "aws_iam_role" "lambda_shutdown_ec2s_role" {
+  name = "lambda_shutdown_ec2s_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "lambda_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["ec2:*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "trigger_shutdown_lambda" {
+  name ="trigger_shutdown_lambda"
+  is_enabled= true
+  schedule_expression = "cron(0 6 ? * * *)"
+}
+
+resource "aws_cloudwatch_event_target" "shutdown_lambda" {
+  arn = aws_lambda_function.shutdown_ec2s.arn
+  rule  = aws_cloudwatch_event_rule.trigger_shutdown_lambda.id
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.shutdown_ec2s.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.trigger_shutdown_lambda.arn
 }
